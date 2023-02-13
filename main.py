2a6eec3e5ed9443daa49620dddccc67884680782
sadamccr <sadamccr@gmail.com>
2023-02-13 12:49:50 +0500

    updated

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 class BST:
     def __init__(self, key):
         self.key = key
-        self.lchild = None
-        self.rchild = None
+        self.l_child = None
+        self.r_child = None
 
 
 root = BST(1)
 print(root.key)
-print(root.lchild)
-print(root.rchild)
+print(root.l_child)
+print(root.r_child)

287262c0392fc1e9a062f642c68112ee53e46d0b
sadamccr <sadamccr@gmail.com>
2023-02-15 13:24:46 +0500

    comments added

--- main.py
+++ main.py
@@ -1,22 +1,22 @@
 class BST:
     def __init__(self, key):
         self.key = key
-        self.l_child = None
-        self.r_child = None
+        self.l_child = None  # it is a pointer having address of subtree to left
+        self.r_child = None  # it is a pointer having address of subtree to right
 
     def insert(self, data):
         if self.key is None:
             self.key = data
             return
-        
+
         if self.key == data:
             return
 
-        if self.key > data:
-            if self.l_child:
-                self.l_child.insert(data)
+        if self.key > data:  # it checks if key is greater than data - subtree goes left
+            if self.l_child:  # it checks if left child of BST is present or not
+                self.l_child.insert(data)  # if present, it inserts data there
             else:
-                self.l_child = BST(data)
+                self.l_child = BST(data)  # if left child is none (none by default) it adds BST - sub tress there
 
         if self.key < data:
             if self.r_child:
@@ -25,7 +25,8 @@ class BST:
                 self.r_child = BST(data)
 
 
-root = BST(None)
-root.insert(10)
-print(root.l_child)
-print(root.r_child)
+root = BST(10)
+list_one = [30, 10, 60, 50, 80, 90, 0, 5]
+
+for i in list_one:
+    root.insert(i)
